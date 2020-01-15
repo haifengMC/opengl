e2d453220623f7f30f3344f471af4fd6ad73d612
haifengMC <1745502795@qq.com>
2020-01-15 20:43:21 +0800

    增加了鼠标点击和移动事件

--- 01_OpenGL概述/01_glfw输出调试信息/main.cpp
+++ 01_OpenGL概述/01_glfw输出调试信息/main.cpp
@@ -1,14 +1,32 @@
+#include <sstream>
+
 #include "vgl.h"
 #include "vapp.h"
 
 BEGIN_APP_DECLARATION(OutputDebugInfo)
 	virtual void OnKey(int key, int scancode, int action, int mods);
+	virtual void OnMouse(int button, int action, int mods);
+	virtual void OnCursor(double x, double y);
 END_APP_DECLARATION()
 DEFINE_APP(OutputDebugInfo, "glfw output debug info")
 
 void OutputDebugInfo::OnKey(int key, int scancode, int action, int mods)
 {
-	char buf[512] = "";
-	wsprintf(buf, "key=%d scancode=%d action=%d mods=%d\n", key, scancode, action, mods);
-	OutputDebugString(buf);
+	std::ostringstream oss;
+	oss << "key=" << key << " scancode=" << scancode << " action=" << action << "mods=" << mods << std::endl;
+	OutputDebugString(oss.str().c_str());
+}
+
+void OutputDebugInfo::OnMouse(int button, int action, int mods)
+{
+	std::ostringstream oss;
+	oss << "button=" << button << " action=" << action << "mods=" << mods << std::endl;
+	OutputDebugString(oss.str().c_str());
+}
+
+void OutputDebugInfo::OnCursor(double x, double y)
+{
+	std::ostringstream oss;
+	oss << "x=" << x << " y=" << y << std::endl;
+	OutputDebugString(oss.str().c_str());
 }
