216f9ce4c3d318446983ead18ccefed01cf89843
haifengMC <1745502795@qq.com>
2020-02-02 22:01:35 +0800

    测试文件拖拽功能

--- 01_OpenGL概述/01_glfw输出调试信息/main.cpp
+++ 01_OpenGL概述/01_glfw输出调试信息/main.cpp
@@ -7,6 +7,7 @@ BEGIN_APP_DECLARATION(OutputDebugInfo)
 	virtual void OnKey(int key, int scancode, int action, int mods);
 	virtual void OnMouse(int button, int action, int mods);
 	virtual void OnCursor(double x, double y);
+	virtual void OnDrop(int num, const char** file);
 END_APP_DECLARATION()
 DEFINE_APP(OutputDebugInfo, "glfw output debug info")
 
@@ -29,4 +30,19 @@ void OutputDebugInfo::OnCursor(double x, double y)
 	std::ostringstream oss;
 	oss << "x=" << x << " y=" << y << std::endl;
 	OutputDebugString(oss.str().c_str());
+}
+
+void OutputDebugInfo::OnDrop(int num, const char** file)
+{
+	for (size_t i = 0; i < num; ++i)
+	{
+		std::ostringstream oss;
+		FILE* pFile;
+		fopen_s(&pFile, file[i], "r");
+		if (pFile) fclose(pFile);
+		else OutputDebugString("null\n");
+		
+		oss << i << " : " << file[i] << std::endl;
+		OutputDebugString(oss.str().c_str());
+	}
 }
