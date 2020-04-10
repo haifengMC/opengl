b1a19f816b9954db11dcd4aef2d5066433424a43
haifengMC <1745502795@qq.com>
2020-04-11 01:25:19 +0800

    绘制线、环线、条带，并通过glLineWidth()设置线宽

--- 03_OpenGL绘制方式/03_绘制线段/main.cpp
+++ 03_OpenGL绘制方式/03_绘制线段/main.cpp
@@ -14,6 +14,7 @@ private:
 	GLuint vao = 0, vbo = 0;
 	GLenum drawModes[3] = { GL_LINES, GL_LINE_LOOP, GL_LINE_STRIP };
 	GLuint drawIdx = 0;
+	GLfloat lineWidth = 5.0f;
 END_APP_DECLARATION()
 DEFINE_APP(DrawLineTest, "draw line test")
 
@@ -46,6 +47,8 @@ void DrawLineTest::Initialize(const char* title /* = 0 */)
 
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
 	glEnableVertexAttribArray(0);
+
+	glLineWidth(lineWidth);
 }
 
 void DrawLineTest::Display(bool auto_redraw /* = true */)
@@ -65,12 +68,15 @@ void DrawLineTest::Finalize()
 void DrawLineTest::OnMouse(int button, int action, int mods)
 {
 	if (GLFW_MOUSE_BUTTON_LEFT == button && GLFW_PRESS == action)
-	{
 		++drawIdx;
-	}
 }
 
 void DrawLineTest::OnScroll(double x, double y)
 {
+	if (lineWidth + y > 1.0f)
+		lineWidth += y;
+	else
+		lineWidth = 1.0f;
 
+	glLineWidth(lineWidth);
 }
