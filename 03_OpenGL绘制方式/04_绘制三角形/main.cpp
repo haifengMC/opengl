214836bad1a97b145092da85bee49f74f258a50b
haifengMC <1745502795@qq.com>
2020-04-28 10:38:12 +0800

    设置多边形的正面、设置裁剪多边形的正面背面还是都裁剪

--- 03_OpenGL绘制方式/04_绘制三角形/main.cpp
+++ 03_OpenGL绘制方式/04_绘制三角形/main.cpp
@@ -10,12 +10,19 @@ BEGIN_APP_DECLARATION(DrawTriangleTest)
 	virtual void Display(bool auto_redraw /* = true */);
 	virtual void Finalize();
 	
+	virtual void OnKey(int key, int scancode, int action, int mods);
 	virtual void OnMouse(int button, int action, int mods);
+	virtual void OnScroll(double x, double y);
 private:
 	GLuint drawId = 0;
 	GLenum draws[3] = { GL_TRIANGLES, GL_TRIANGLE_FAN, GL_TRIANGLE_STRIP };
 	GLuint modeId = 0;
 	GLenum modes[3] = { GL_FILL, GL_LINE, GL_POINT };
+	GLboolean isCull = GL_FALSE;
+	GLuint cullId = 0;
+	GLenum culls[3] = { GL_FRONT, GL_BACK, GL_FRONT_AND_BACK };
+	GLuint faceId = 0;
+	GLenum faces[2] = { GL_CCW, GL_CW };
 
 	GLfloat vertices[12] =
 	{
@@ -60,8 +67,14 @@ void DrawTriangleTest::Initialize(const char* title /* = 0 */)
 void DrawTriangleTest::Display(bool auto_redraw /* = true */)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBindVertexArray(vao);
+	glBindVertexArray(vao);				
 	glPolygonMode(GL_FRONT_AND_BACK, modes[modeId % countof(modes)]);
+	glFrontFace(faces[faceId % countof(faces)]);
+	if (isCull % 2)
+		glEnable(GL_CULL_FACE);
+	else
+		glDisable(GL_CULL_FACE);
+	glCullFace(culls[cullId % countof(culls)]);
 	glDrawArrays(draws[drawId % countof(draws)], 0, countof(vertices) / 2);
 
 	base::Display(auto_redraw);
@@ -72,10 +85,55 @@ void DrawTriangleTest::Finalize()
 
 }
 
+void DrawTriangleTest::OnKey(int key, int scancode, int action, int mods)
+{
+	switch (key)
+	{
+	case GLFW_KEY_Z:
+		{
+			if (GLFW_PRESS == action)
+				++faceId;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
 void DrawTriangleTest::OnMouse(int button, int action, int mods)
 {
-	if (GLFW_MOUSE_BUTTON_LEFT == button && GLFW_PRESS == action)
-		++drawId;
-	if (GLFW_MOUSE_BUTTON_RIGHT == button && GLFW_PRESS == action)
-		++modeId;
+	switch (button)
+	{
+	case GLFW_MOUSE_BUTTON_LEFT:
+		{
+			if (GLFW_PRESS == action)
+				++drawId;
+		}
+		break;
+	case GLFW_MOUSE_BUTTON_RIGHT:
+		{
+			if (GLFW_PRESS == action)
+				++modeId;
+		}
+		break;
+	case GLFW_MOUSE_BUTTON_MIDDLE:
+		{
+			if (GLFW_PRESS == action)
+				++isCull;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+void DrawTriangleTest::OnScroll(double x, double y)
+{
+	if (y >= 0 || cullId >= -y)
+	{
+		cullId += y;
+		return;
+	}
+
+	cullId += countof(culls) + y;
 }
