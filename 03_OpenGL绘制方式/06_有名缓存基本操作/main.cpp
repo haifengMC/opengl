01fb417465ea9c45ecbe82496b4c5b9fbad4a911
haifengMC <1745502795@qq.com>
2020-05-05 22:29:28 +0800

    保存新的二进制着色器文件、修改有名缓存的若干bug

--- 03_OpenGL绘制方式/06_有名缓存基本操作/main.cpp
+++ 03_OpenGL绘制方式/06_有名缓存基本操作/main.cpp
@@ -8,7 +8,7 @@
 #define INVALID_COORD -2.0f
 typedef std::pair<GLfloat, GLfloat> Point;
 
-BEGIN_APP_DECLARATION(UnnamedBufferOperation)
+BEGIN_APP_DECLARATION(NamedBufferOperation)
 virtual void Initialize(const char* title /* = 0 */);
 virtual void Display(bool auto_redraw /* = true */);
 virtual void Finalize();
@@ -35,162 +35,166 @@ private:
 
 	void refreshPointId(const Point& cursor);
 	GLboolean surroundPoint(const Point& p1, const Point& p2, const GLfloat& diff = 0.05f);
-	END_APP_DECLARATION()
+END_APP_DECLARATION()
 
-	DEFINE_APP(UnnamedBufferOperation, "unnamed buffer operation")
+DEFINE_APP(NamedBufferOperation, "named buffer operation")
 
-	void UnnamedBufferOperation::Initialize(const char* title /* = 0 */)
+void NamedBufferOperation::Initialize(const char* title /* = 0 */)
+{
+	base::Initialize(title);
+
+	ErrMsg errMsg = { GLLS_ERRMSG_ALL };
+	GLuint prog = LoadProgramByBinary("NamedBufferOperation.bin", &errMsg);
+	if (errMsg.err())
 	{
-		base::Initialize(title);
-
-		ErrMsg errMsg = { GLLS_ERRMSG_ALL };
-		GLuint prog = LoadProgramByBinary("NamedBufferOperation.bin", &errMsg);
-		if (errMsg.err())
-		{
-			OutputDebugStringA(errMsg.msg.c_str());
-			return;
-		}
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
-
-		glGenBuffers(2, vbo);
-		glNamedBufferStorage(vbo[0], sizeof(triangle) + sizeof(color), triangle, GL_DYNAMIC_STORAGE_BIT);
-		glNamedBufferSubData(vbo[0], sizeof(triangle), sizeof(color), color);
-		glNamedBufferStorage(vbo[1], sizeof(triangle), triangle, 0);
-
-		glUseProgram(prog);
-
-		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)sizeof(triangle));
-		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-
-		glPointSize(10.0f);
+		OutputDebugStringA(errMsg.msg.c_str());
+		return;
 	}
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-	void UnnamedBufferOperation::Display(bool auto_redraw /* = true */)
+	glGenBuffers(2, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBindBuffer(GL_COPY_READ_BUFFER, vbo[1]);
+	glNamedBufferStorage(vbo[0], sizeof(triangle) + sizeof(color), triangle, GL_DYNAMIC_STORAGE_BIT);
+	glNamedBufferSubData(vbo[0], sizeof(triangle), sizeof(color), color);
+	glNamedBufferStorage(vbo[1], sizeof(triangle), triangle, 0);
+
+	glUseProgram(prog);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)sizeof(triangle));
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	glPointSize(10.0f);
+}
+
+void NamedBufferOperation::Display(bool auto_redraw /* = true */)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	if (-1 != pointId)
+		glDrawArrays(GL_POINTS, pointId, 1);
+
+	base::Display(auto_redraw);
+}
+
+void NamedBufferOperation::Finalize()
+{
+
+}
+
+
+void NamedBufferOperation::OnMouse(int button, int action, int mods)
+{
+	switch (button)
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
-		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		if (-1 != pointId)
-			glDrawArrays(GL_POINTS, pointId, 1);
-
-		base::Display(auto_redraw);
-	}
-
-	void UnnamedBufferOperation::Finalize()
+	case GLFW_MOUSE_BUTTON_LEFT:
 	{
-
-	}
-
-
-	void UnnamedBufferOperation::OnMouse(int button, int action, int mods)
-	{
-		switch (button)
+		switch (action)
 		{
-		case GLFW_MOUSE_BUTTON_LEFT:
+		case  GLFW_PRESS:
 		{
-			switch (action)
-			{
-			case  GLFW_PRESS:
-			{
-				isPress = GL_TRUE;
-			}
-			break;
-			case GLFW_RELEASE:
-			{
-				isPress = GL_FALSE;
-				oldX = oldY = INVALID_COORD;
-			}
-			break;
-			default:
-				break;
-			}
+			isPress = GL_TRUE;
 		}
 		break;
-		case GLFW_MOUSE_BUTTON_RIGHT:
+		case GLFW_RELEASE:
 		{
-			if (GLFW_PRESS == action)
-				glCopyNamedBufferSubData(vbo[1], vbo[0], 0, 0, sizeof(triangle));
-		}
-		break;
-		case GLFW_MOUSE_BUTTON_MIDDLE:
-		{
-			if (GLFW_PRESS == action)
-			{
-				GLfloat tmpTriangle[6];
-				glGetNamedBufferSubData(vbo[0], 0, sizeof(tmpTriangle), tmpTriangle);
-				std::stringstream os;
-				for (GLfloat f : tmpTriangle)
-					os << f << " ";
-				OutputDebugStringA(os.str().c_str());
-			}
+			isPress = GL_FALSE;
+			oldX = oldY = INVALID_COORD;
 		}
 		break;
 		default:
 			break;
 		}
 	}
-
-	void UnnamedBufferOperation::OnCursorNorm(float x, float y)
+	break;
+	case GLFW_MOUSE_BUTTON_RIGHT:
 	{
-		if (!isPress)
+		if (GLFW_PRESS == action)
 		{
-			GLint oldPointId = pointId;
-			refreshPointId({ x, y });
-			if (oldPointId != pointId)
-			{
-				char buf[64] = "";
-				sprintf_s(buf, "%d\n", pointId);
-				OutputDebugStringA(buf);
-			}
+			glCopyNamedBufferSubData(vbo[1], vbo[0], 0, 0, sizeof(triangle));
+			glGetNamedBufferSubData(vbo[0], 0, sizeof(triangle), triangle);
 		}
-
-		if (!isPress || -1 == pointId) return;
-
-		if (INVALID_COORD == oldX || INVALID_COORD == oldY)
+	}
+	break;
+	case GLFW_MOUSE_BUTTON_MIDDLE:
+	{
+		if (GLFW_PRESS == action)
 		{
-			oldX = x;
-			oldY = y;
-			return;
+			GLfloat tmpTriangle[6];
+			glGetNamedBufferSubData(vbo[0], 0, sizeof(tmpTriangle), tmpTriangle);
+			std::stringstream os;
+			for (GLfloat f : tmpTriangle)
+				os << f << " ";
+			OutputDebugStringA(os.str().c_str());
 		}
+	}
+	break;
+	default:
+		break;
+	}
+}
 
-		GLfloat(&p)[2] = triangle[pointId];
-		p[0] += x - oldX;
-		p[1] += y - oldY;
+void NamedBufferOperation::OnCursorNorm(float x, float y)
+{
+	if (!isPress)
+	{
+		GLint oldPointId = pointId;
+		refreshPointId({ x, y });
+		if (oldPointId != pointId)
+		{
+			char buf[64] = "";
+			sprintf_s(buf, "%d\n", pointId);
+			OutputDebugStringA(buf);
+		}
+	}
+
+	if (!isPress || -1 == pointId) return;
+
+	if (INVALID_COORD == oldX || INVALID_COORD == oldY)
+	{
 		oldX = x;
 		oldY = y;
-		glClearNamedBufferSubData(vbo[0], GL_RG32F, sizeof(p) * pointId, sizeof(p), GL_RG, GL_FLOAT, p);
+		return;
 	}
 
-	void UnnamedBufferOperation::refreshPointId(const Point& cursor)
-	{
-		if (-1 != pointId && !surroundPoint({ triangle[pointId][0], triangle[pointId][1] }, cursor))
-		{
-			pointId = -1;
-			return;
-		}
+	GLfloat(&p)[2] = triangle[pointId];
+	p[0] += x - oldX;
+	p[1] += y - oldY;
+	oldX = x;
+	oldY = y;
+	glClearNamedBufferSubData(vbo[0], GL_RG32F, sizeof(p) * pointId, sizeof(p), GL_RG, GL_FLOAT, p);
+}
 
-		pointId = 0;
-		for (GLfloat(&p)[2] : triangle)
-		{
-			if (surroundPoint({ p[0], p[1] }, cursor))
-				return;
-			++pointId;
-		}
+void NamedBufferOperation::refreshPointId(const Point& cursor)
+{
+	if (-1 != pointId && !surroundPoint({ triangle[pointId][0], triangle[pointId][1] }, cursor))
+	{
 		pointId = -1;
+		return;
 	}
 
-	GLboolean UnnamedBufferOperation::surroundPoint(const Point& p1, const Point& p2, const GLfloat& diff)
+	pointId = 0;
+	for (GLfloat(&p)[2] : triangle)
 	{
-		GLfloat
-			distX = p1.first - p2.first,
-			distY = p1.second - p2.second;
-
-		GLfloat distance = distX * distX + distY * distY;
-		GLfloat distDiff = diff * diff;
-
-		return distance < distDiff ? GL_TRUE : GL_FALSE;
+		if (surroundPoint({ p[0], p[1] }, cursor))
+			return;
+		++pointId;
 	}
+	pointId = -1;
+}
+
+GLboolean NamedBufferOperation::surroundPoint(const Point& p1, const Point& p2, const GLfloat& diff)
+{
+	GLfloat
+		distX = p1.first - p2.first,
+		distY = p1.second - p2.second;
+
+	GLfloat distance = distX * distX + distY * distY;
+	GLfloat distDiff = diff * diff;
+
+	return distance < distDiff ? GL_TRUE : GL_FALSE;
+}
